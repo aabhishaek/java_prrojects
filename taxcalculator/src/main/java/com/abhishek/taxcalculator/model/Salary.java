b9cabd235c606c6bdbc351dad5b02ebab87a09d2
aabhishaek <abhishekcs007@gmail.com>
2023-06-15 23:36:49 +0530

    Redo commit - Add method to calculate total salary, test case was added in last commit

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/model/Salary.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/model/Salary.java
@@ -20,4 +20,20 @@ public class Salary {
     private BigDecimal hra;
     private BigDecimal specialAllowance;
     private Map<String, BigDecimal> otherAllowances;
+
+    public BigDecimal getTotal() {
+        BigDecimal totalSalary = BigDecimal.ZERO;
+
+        BigDecimal otherAllowancesTotal = BigDecimal.ZERO;
+        for (String allowance : otherAllowances.keySet()) {
+            otherAllowancesTotal = otherAllowancesTotal.add(otherAllowances.get(allowance));
+        }
+
+        totalSalary = totalSalary.add(basic);
+        totalSalary = totalSalary.add(hra);
+        totalSalary = totalSalary.add(specialAllowance);
+        totalSalary = totalSalary.add(otherAllowancesTotal);
+
+        return totalSalary;
+    }
 }

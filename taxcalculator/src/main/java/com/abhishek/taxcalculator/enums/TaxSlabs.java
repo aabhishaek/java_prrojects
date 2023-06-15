3005ab38283621e24b44c597e03b76678d542390
aabhishaek <abhishekcs007@gmail.com>
2023-06-15 23:35:03 +0530

    Add tax slabs for old and new regime for calculation of tax

---
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/enums/TaxSlabs.java
@@ -0,0 +1,46 @@
+package com.abhishek.taxcalculator.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.abhishek.taxcalculator.enums.Regime.NEW_REGIME;
+import static com.abhishek.taxcalculator.enums.Regime.OLD_REGIME;
+
+@Getter
+@AllArgsConstructor
+public enum TaxSlabs {
+
+    // OLD REGIME
+    SLAB_1_OLD(0, 250000 , 0, OLD_REGIME),
+    SLAB_2_OLD(250000, 500000, 5, OLD_REGIME),
+    SLAB_3_OLD(500000, 1000000, 20, OLD_REGIME),
+    SLAB_4_OLD(1000000, Integer.MAX_VALUE,30, OLD_REGIME),
+
+    // New Regime
+    SLAB_1_NEW(0, 300000, 0, NEW_REGIME),
+    SLAB_2_NEW(300000, 600000, 5, NEW_REGIME),
+    SLAB_3_NEW(600000, 900000, 10, NEW_REGIME),
+    SLAB_4_NEW(900000, 1200000, 15, NEW_REGIME),
+    SLAB_5_NEW(1200000, 1500000, 20, NEW_REGIME),
+    SLAB_6_NEW(1500000, Integer.MAX_VALUE, 30, NEW_REGIME);
+
+    private int minSalaryRange;
+    private int maxSalaryRange;
+    private int percentageOfTax;
+    private Regime regime;
+
+    public static List<TaxSlabs> getSlabsForGivenRegime(Regime regime) {
+        List<TaxSlabs> slabs = new ArrayList<>();
+
+        for (TaxSlabs slab : TaxSlabs.values()) {
+            if (slab.getRegime() == regime) {
+                slabs.add(slab);
+            }
+        }
+
+        return slabs;
+    }
+}

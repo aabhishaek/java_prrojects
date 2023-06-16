a5eafeb34a374212e7567da3d2b2fdaa533f1685
aabhishaek <abhishekcs007@gmail.com>
2023-06-16 23:34:34 +0530

    Develop initial logic for tax calculation with old regime

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/util/TaxCalculator.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/util/TaxCalculator.java
@@ -9,16 +9,48 @@ import java.util.List;
 
 public class TaxCalculator {
 
-    public BigDecimal getTotalTaxableIncomeFromSalary(Salary salary) {
+    private static FINAL BigDecimal = new BigDecimal(50000);
+
+    /**
+     * Calculates the total taxable income based on the given salary
+     * (Taxable income is by default calculated for old_slab)
+     *
+     * @param salary
+     * @return BigDecimal - Total Taxable Income
+     */
+    public BigDecimal calculateTotalTaxableIncomeFromSalary(Salary salary) {
         BigDecimal totalTaxableIncome = BigDecimal.ZERO;
 
         BigDecimal totalSalary = salary.getTotal();
         List<TaxSlabs> slabs = TaxSlabs.getSlabsForGivenRegime(Regime.OLD_REGIME);
 
-        for (TaxSlabs slab: 
-             ) {
+        for (TaxSlabs slab : slabs) {
+            BigDecimal payableTaxForSlab = BigDecimal.ZERO;
 
+            if (totalSalary.compareTo(BigDecimal.ZERO) <= 0) {
+                break;
+            }
+
+            // Subtract Standard deductions
+
+            if (slab.getPercentageOfTax() > 0) {
+                if (new BigDecimal(slab.getMaxSalaryRange()).compareTo(totalSalary) >= 0) {
+                    payableTaxForSlab = totalSalary.multiply(new BigDecimal(slab.getPercentageOfTax() / 100));
+                } else {
+
+                    if (slab.getMaxSalaryRange() == Integer.MAX_VALUE) {
+                        payableTaxForSlab = totalSalary.multiply(new BigDecimal(slab.getPercentageOfTax() / 100));
+                    } else {
+                        payableTaxForSlab = new BigDecimal(slab.getMaxSalaryRange())
+                                .multiply(new BigDecimal(slab.getPercentageOfTax() / 100));
+                    }
+                }
+                totalTaxableIncome = totalTaxableIncome.add(payableTaxForSlab);
+            }
+            totalSalary = totalSalary.subtract(new BigDecimal(slab.getMaxSalaryRange()));
         }
+
+        return totalTaxableIncome;
 
     }
 }

5e3ecb2b177fa36329c067de8758642b5e953ef5
aabhishaek <abhishekcs007@gmail.com>
2023-06-17 19:40:58 +0530

    Refactor tax slab enum

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/enums/TaxSlab.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/enums/TaxSlab.java
@@ -14,22 +14,22 @@ import static com.abhishek.taxcalculator.enums.Regime.OLD_REGIME;
 public enum TaxSlab {
 
     // OLD REGIME
-    SLAB_1_OLD(0, 250000 , 0, OLD_REGIME),
-    SLAB_2_OLD(250000, 500000, 5, OLD_REGIME),
-    SLAB_3_OLD(500000, 1000000, 20, OLD_REGIME),
-    SLAB_4_OLD(1000000, Integer.MAX_VALUE,30, OLD_REGIME),
+    SLAB_1_OLD(250000 , 0, 0,OLD_REGIME),
+    SLAB_2_OLD(500000, 5, 12500,OLD_REGIME),
+    SLAB_3_OLD(1000000, 20, 100000,OLD_REGIME),
+    SLAB_4_OLD(0,30, 0, OLD_REGIME),
 
     // New Regime
-    SLAB_1_NEW(0, 300000, 0, NEW_REGIME),
-    SLAB_2_NEW(300000, 600000, 5, NEW_REGIME),
-    SLAB_3_NEW(600000, 900000, 10, NEW_REGIME),
-    SLAB_4_NEW(900000, 1200000, 15, NEW_REGIME),
-    SLAB_5_NEW(1200000, 1500000, 20, NEW_REGIME),
-    SLAB_6_NEW(1500000, Integer.MAX_VALUE, 30, NEW_REGIME);
+    SLAB_1_NEW(300000, 0, 0, NEW_REGIME),
+    SLAB_2_NEW(600000, 5, 15000, NEW_REGIME),
+    SLAB_3_NEW(900000, 10, 30000, NEW_REGIME),
+    SLAB_4_NEW(1200000, 15, 45000, NEW_REGIME),
+    SLAB_5_NEW(1500000, 20, 60000, NEW_REGIME),
+    SLAB_6_NEW(0, 30, 0, NEW_REGIME);
 
-    private int minSalaryRange;
-    private int maxSalaryRange;
+    private int upperLimit;
     private int percentageOfTax;
+    private int maxTax;
     private Regime regime;
 
     public static List<TaxSlab> getSlabsForGivenRegime(Regime regime) {

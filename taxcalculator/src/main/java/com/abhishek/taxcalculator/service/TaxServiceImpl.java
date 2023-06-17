5c396499332430eca09f92846fde16e317aca9bd
aabhishaek <abhishekcs007@gmail.com>
2023-06-17 19:43:33 +0530

    Add method in tax service to calculate tax. Add relevant test cases

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/service/TaxServiceImpl.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/service/TaxServiceImpl.java
@@ -1,16 +1,22 @@
 package com.abhishek.taxcalculator.service;
 
 import com.abhishek.taxcalculator.enums.Regime;
+import com.abhishek.taxcalculator.model.Salary;
 import com.abhishek.taxcalculator.model.Tax;
+import com.abhishek.taxcalculator.util.TaxCalculator;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class TaxServiceImpl implements TaxService {
 
     private final Tax tax;
+    private final SalaryService salaryService;
 
-    public TaxServiceImpl(Tax tax) {
+    public TaxServiceImpl(Tax tax, SalaryService salaryService) {
         this.tax = tax;
+        this.salaryService = salaryService;
     }
 
     @Override
@@ -21,5 +27,13 @@ public class TaxServiceImpl implements TaxService {
     @Override
     public Regime getRegime() {
         return tax.getRegime();
+    }
+
+    @Override
+    public BigDecimal calculateTotalTaxableIncome(String userId) {
+        Salary salary = salaryService.getSalary(userId);
+
+        TaxCalculator calculator = new TaxCalculator();
+        return calculator.calculateTotalTaxableIncomeFromSalary(salary);
     }
 }

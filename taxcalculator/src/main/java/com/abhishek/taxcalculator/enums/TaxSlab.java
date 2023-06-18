b2cb41c377975b502e7505abf15b180ba552c639
aabhishaek <abhishekcs007@gmail.com>
2023-06-18 14:32:00 +0530

    Refactor Tax Slab enum to add methods which help to avoid boiler plate code in places where the getters are used

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/enums/TaxSlab.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/enums/TaxSlab.java
@@ -1,23 +1,23 @@
 package com.abhishek.taxcalculator.enums;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.abhishek.taxcalculator.enums.Regime.NEW_REGIME;
 import static com.abhishek.taxcalculator.enums.Regime.OLD_REGIME;
 
-@Getter
 @AllArgsConstructor
 public enum TaxSlab {
 
     // OLD REGIME
-    SLAB_1_OLD(250000 , 0, 0,OLD_REGIME),
-    SLAB_2_OLD(500000, 5, 12500,OLD_REGIME),
-    SLAB_3_OLD(1000000, 20, 100000,OLD_REGIME),
-    SLAB_4_OLD(0,30, 0, OLD_REGIME),
+    SLAB_1_OLD(250000, 0, 0, OLD_REGIME),
+    SLAB_2_OLD(500000, 5, 12500, OLD_REGIME),
+    SLAB_3_OLD(1000000, 20, 100000, OLD_REGIME),
+    SLAB_4_OLD(0, 30, 0, OLD_REGIME),
 
     // New Regime
     SLAB_1_NEW(300000, 0, 0, NEW_REGIME),
@@ -33,14 +33,24 @@ public enum TaxSlab {
     private Regime regime;
 
     public static List<TaxSlab> getSlabsForGivenRegime(Regime regime) {
-        List<TaxSlab> slabs = new ArrayList<>();
+        return Stream.of(TaxSlab.values())
+                .filter(slab -> slab.getRegime() == regime)
+                .collect(Collectors.toList());
+    }
 
-        for (TaxSlab slab : TaxSlab.values()) {
-            if (slab.getRegime() == regime) {
-                slabs.add(slab);
-            }
-        }
+    public BigDecimal getUpperLimit() {
+        return new BigDecimal(upperLimit);
+    }
 
-        return slabs;
+    public BigDecimal getPercentageOfTax() {
+        return new BigDecimal(percentageOfTax);
+    }
+
+    public BigDecimal getMaxTax() {
+        return new BigDecimal(maxTax);
+    }
+
+    public Regime getRegime() {
+        return regime;
     }
 }

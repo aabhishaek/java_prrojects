75682b2f3394d6ad7da54bca551077fe638231a1
aabhishaek <abhishekcs007@gmail.com>
2023-06-30 22:39:33 +0530

    Investment model class, which will hold each section data

---
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/model/Investment.java
@@ -0,0 +1,12 @@
+package com.abhishek.taxcalculator.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Investment {
+
+    private Section section80c;
+    private Section section80d;
+}

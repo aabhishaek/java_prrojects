517a08e8f45d20923f76974230ced470ea245855
aabhishaek <abhishekcs007@gmail.com>
2023-07-07 16:00:39 +0530

    Add config to support i18n

---
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/config/ResourceConfig.java
@@ -0,0 +1,26 @@
+package com.abhishek.taxcalculator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class ResourceConfig {
+
+    @Bean
+    public AcceptHeaderLocaleResolver localeResolver() {
+        final AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.US);
+        return resolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("resources");
+        return source;
+    }
+}

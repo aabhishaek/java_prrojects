fc229c0b2808dedd5ec39595bfab6be3f26be3ca
aabhishaek <abhishekcs007@gmail.com>
2023-07-09 09:54:50 +0530

    Refactor tax controller and add resources properties file

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/controller/TaxController.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/controller/TaxController.java
@@ -22,24 +22,24 @@ public class TaxController extends BaseController{
         this.taxService = taxService;
     }
 
+    /**
+     * Endpoint to choose the Tax Regime based on which tax will be calculated
+     *
+     * @param "Accepts" regime param as enum (OLD_REGIME, NEW REGIME)
+     * @return Returns text response indicating regime has been successfully set
+     */
     @PostMapping(value = "/choose-regime", consumes = "application/json", produces = "application/json")
     public ResponseEntity<APIResponse> chooseRegime(@RequestBody Regime regime) {
         taxService.chooseRegime(regime);
         return wrapResponse(null, "tax.regime.chosen.message", 200);
     }
 
+    /**
+     *  Endpoint to GET the chosen regime
+      * @return Chosen regime
+     */
     @GetMapping(value = "/regime", produces = "application/json")
     public ResponseEntity<APIResponse> getRegime() {
-
-        APIResponse response = new APIResponse();
-        response.setData(taxService.getRegime());
-        return ResponseEntity.ok().body(response);
-    }
-
-    @GetMapping(value = "/investments", produces = "application/json")
-    public ResponseEntity<APIResponse> getInvestments() {
-
-
-        return ResponseEntity.ok().body(new APIResponse());
+        return wrapResponse(taxService.getRegime());
     }
 }

a5c89d03585a995c1368ead814181f11aceb7fd5
aabhishaek <abhishekcs007@gmail.com>
2023-08-16 18:03:59 +0530

    Refactor salary controller, Add Salary DTO,add labels for salary form properties in resources file

--- taxcalculator/src/main/java/com/abhishek/taxcalculator/controller/SalaryController.java
+++ taxcalculator/src/main/java/com/abhishek/taxcalculator/controller/SalaryController.java
@@ -1,6 +1,7 @@
 package com.abhishek.taxcalculator.controller;
 
 import com.abhishek.taxcalculator.model.Salary;
+import com.abhishek.taxcalculator.model.SalaryDTO;
 import com.abhishek.taxcalculator.service.SalaryService;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Controller containing end points to perform salary related operations
@@ -44,9 +50,28 @@ public class SalaryController extends BaseController{
      * @return Salary Object
      */
     @GetMapping("/{userId}")
-    public ResponseEntity<Salary> getSalary(@PathVariable String userId) {
+    public ResponseEntity<List<Object>> getSalary(@PathVariable String userId) {
         Salary salary = salaryService.getSalary(userId);
-        return wrapResponse(salary);
+
+        List<Object> responseList = new ArrayList<>();
+        responseList.add(new SalaryDTO("basic", "basic", getRes("salary.basic.label"), salary.getBasic()));
+        responseList.add(new SalaryDTO("hra", "hra", getRes("salary.hra.label"), salary.getHra()));
+        responseList.add(new SalaryDTO("spAll", "specialAllowance", getRes("salary.sa.label"), salary.getSpecialAllowance()));
+        responseList.add(new SalaryDTO("eepf", "employerEPFContribution", getRes("salary.eepf.label"), salary.getEmployerEPFContribution()));
+
+        Map<String, BigDecimal> otherAllowances = salary.getOtherAllowances();
+        List<Object> otherAllowancesObj = new ArrayList<>();
+        if (otherAllowances != null) {
+
+            for (Map.Entry<String, BigDecimal> entry: otherAllowances.entrySet()) {
+                otherAllowancesObj.add(new SalaryDTO(entry.getKey().replaceAll("\\s+", "").toLowerCase(), entry.getKey(),
+                                               entry.getKey(), entry.getValue()));
+            }
+        }
+        responseList.add(new SalaryDTO("otherAllowances", "", "", otherAllowancesObj));
+
+
+        return wrapResponse(responseList);
     }
 
 }
